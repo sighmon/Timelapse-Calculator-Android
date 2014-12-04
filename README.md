8eda83a1967bc1c21b490c06bae70d307b34ceae
sighmon <simon.loffler@gmail.com>
2014-12-04 12:25:49 +1030

    First commit.

---
+++ README.md
@@ -0,0 +1,38 @@
+# TIMELAPSE HELPER
+<http://sighmon.com/timelapse-helper>
+
+## What's New?
+Version 1.0
+
+* Android version!
+
+### iOS
+
+* See the [iOS repository](https://github.com/sighmon/Timelapse-Calculator).
+
+
+### Interval Calculator mode
+This is a test feature I've added so you can calculate the Interval time (time in seconds between photos) by spinning the Shooting wheel. You'll notice that as you spin the wheel, the calculator automatically adjusts it to the closest shooting time available. This is because you can't select part seconds for the interval.
+If you notice any strange things, or have any suggestions for improvements, please email me <sighmon(at)sighmon.com>
+
+## Description
+The Timelapse Helper iPhone app is a companion for anyone interested in timelapse photography.
+
+It's a simple app to help you calculate the shooting time needed to create timelapse footage of a desired playback duration.
+
+Set the interval time (in seconds) between photos, the frames per second that you'd like in the finished video and the app does the math for you.
+
+There's even a little email button to send your calculations to your friends or camera crew.
+
+Based on the original Python code for PC written by Dan Thompson, this iPhone app is FREE software released under a GPL license.
+
+Written and designed by Simon Loffler.
+
+Special thanks to Pix for his help with optimisation and bug fixes.
+
+## Copyright
+Copyright (c) 2011, Simon Loffler
+All rights reserved.
+
+## License
+This software is made available under the terms of the simplified GPL license.
---
+++ mobile/src/main/java/com/sighmon/timelapsehelper/MainActivity.java
@@ -0,0 +1,110 @@
+package com.sighmon.timelapsehelper;
+
+import android.app.Activity;
+import android.app.ActionBar;
+import android.app.AlertDialog;
+import android.app.Fragment;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+import android.widget.EditText;
+
+public class MainActivity extends Activity {
+
+    public final static String EXTRA_MESSAGE = "com.sighmon.timelapsehelper.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        switch (item.getItemId()) {
+            case R.id.menu_search:
+                Log.i("Search", "Search pressed.");
+                openSearch();
+                return true;
+            case R.id.action_settings:
+//                openSettings();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void openSearch() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        // Add the buttons
+        builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // User clicked OK button
+                Log.i("Search", "Clicked OK.");
+            }
+        });
+        builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // User cancelled the dialog
+                Log.i("Search", "Clicked cancel.");
+            }
+        });
+        // Set other dialog properties
+        builder.setMessage(R.string.dialog_message)
+                .setTitle(R.string.dialog_title);
+
+        // Create the AlertDialog
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            return rootView;
+        }
+    }
+
+    /** Called when the user clicks the Send button */
+    public void sendMessage(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+}
+
