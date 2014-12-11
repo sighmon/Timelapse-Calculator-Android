fc0943da19b8cab9231aaded9d0b5320f87e09be
sighmon <simon.loffler@gmail.com>
2014-12-11 12:51:11 +1030

    Added picker descriptions. Removed tutorial gumf.

--- mobile/src/main/java/com/sighmon/timelapsehelper/MainActivity.java
+++ mobile/src/main/java/com/sighmon/timelapsehelper/MainActivity.java
@@ -159,14 +159,14 @@ public class MainActivity extends Activity {
     }
 
     /** Called when the user clicks the Send button */
-    public void sendMessage(View view) {
+    /* public void sendMessage(View view) {
         // Do something in response to button
         Intent intent = new Intent(this, DisplayMessageActivity.class);
         EditText editText = (EditText) findViewById(R.id.edit_message);
         String message = editText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
-    }
+    } */
 
     /*** TIME CALCULATION MATH ***/
 
